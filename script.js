0ccc71632032aa72267b4f6fb97039b9a2e596e6
AlejandroZapien <alejandrozapien09@gmail.com>
2022-11-07 04:55:31 -0600

    Tweak movement speed and ball physics

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 var position = 0
-$(window).on('keydown', (e) => {
-
+function move (e) {
     if (e.key === 'ArrowDown' && parseInt($('#player1').css('top')) < 600 - parseInt($('#player1').css('height'))) {
-        position += 5
-        console.log('Down')
+        position += 10
     }
     if (e.key === 'ArrowUp' && parseInt($('#player1').css('top')) > 0) {
-        position -= 5
-        console.log('Up')
+        position -= 10
     }
-    console.log($('#player1').css('top'))
-
     $('#player1').css('top', position)
+}
 
-
-})
+$(window).on('keydown', move)
 
 //Ball physics
 var ballX = parseInt($('#ball').css('left'))
 var ballY = parseInt($('#ball').css('top'))
-var velocityX = 5
-var velocityY = 5
+var velocityX = 1
+var velocityY = 1
 var ballInterval = setInterval(() => {
+    //change the X and Y position of the ball by the velocity in the X and Y direction
     ballX += velocityX
     ballY += velocityY
-    //Reverse velocity if left or right bounds is hit
+    //Reverse velocityX if left or right bounds is hit
     if(ballX > 600 || ballX < 0) {
         velocityX = -velocityX
     }
+    //Reverse velocityY if top or bottom bounds is hit
     if(ballY > 600 || ballY < 0) {
         velocityY = -velocityY
     }
-    //ballY += velocity
+
+    //Update the position of the ball
     $('#ball').css('left', ballX)
     $('#ball').css('top', ballY)
 
-}, 50)
+}, 1)
