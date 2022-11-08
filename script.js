5653ad7032f115f48a6ee92fe27c22e2eb9fa1ae
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-07 18:46:44 -0600

    Add Player score functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var player1Position = 0
 var player2Position = 0
+var player1Score = 0
+var player2Score = 0
+
 function move(e) {
     if (e.key === 's' && parseInt($('#player1').css('top')) < 600 - parseInt($('#player1').css('height'))) {
         player1Position += 10
@@ -38,14 +41,19 @@ var ballX = parseInt($('#ball').css('left'))
 var ballY = parseInt($('#ball').css('top'))
 var velocityX = 2
 var velocityY = 2
-var ballInterval = setInterval(() => {
+
+function startBall() {
+    $('#ball').show()
     //change the X and Y position of the ball by the velocity in the X and Y direction
     ballX += velocityX
     ballY += velocityY
-    //Reverse velocityX if left or right bounds is hit
-    //change this to score
-    if (ballX < leftBound || ballX > rightBound - ballWidth) {
-        velocityX = -velocityX
+    //If ballX goes beyond leftBound, player 2 scores
+    if (ballX < leftBound) {
+        score('player 2')
+    }
+    //If ballX goes beyond rightBound, (accounting for width of the ball), player 1 scores
+    if (ballX > rightBound - ballWidth) {
+        score('player 1')
     }
     //Reverse velocityY if top or bottom bounds is hit
     //change this to score
@@ -66,5 +74,29 @@ var ballInterval = setInterval(() => {
     //Update the position of the ball
     $('#ball').css('left', ballX)
     $('#ball').css('top', ballY)
+}
+var ballInterval = setInterval(startBall, 10)
 
-}, 10)
+function score(player) {
+    if (player === "player 1") {
+        //increase player 1 score
+        player1Score++
+        $('#player1-score').text(player1Score)
+    } else if (player === "player 2") {
+        //increase player 2 score
+        player2Score++
+        $('#player2-score').text(player2Score)
+    }
+
+    //Stop ball function
+    clearInterval(ballInterval)
+    //Hide ball
+    $('#ball').hide()
+    //After 1 second, launch another ball
+    setTimeout(() => {
+        ballX = 300
+        ballY = 200
+        ballInterval = setInterval(startBall, 10)
+    }, 1000)
+
+}
